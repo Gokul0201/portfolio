8c9867755374a78d5910888b27821ae6d7e8037b
Gokul0201 <kgokula314@gmail.com>
2022-09-20 10:24:48 +0530

    emailjs

--- src/Components/contact/Contact.js
+++ src/Components/contact/Contact.js
@@ -2,22 +2,23 @@ import {React}from 'react'
 import './contact.css'
 import {MdEmail} from 'react-icons/md'
 import {BsWhatsapp} from 'react-icons/bs'
-// import  { useRef } from 'react';
-// import emailjs from 'emailjs-com';
+import  { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 
 const Contact = () => {
-  // const form = useRef();
+  const form = useRef();
 
-  // const sendEmail = (e) => {
-  //   e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-  //   emailjs.sendForm('service_kbmh0hc', 'template_llrg94n', form.current, '6GgLdpdCE6_CNFTpH')
-  //     .then((result) => {
-  //         console.log(result.text);
-  //     }, (error) => {
-  //         console.log(error.text);
-  //     });
+    emailjs.sendForm('service_kbmh0hc', 'template_llrg94n', form.current,'6GgLdpdCE6_CNFTpH')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
    
   return (
     <section id="contact">
@@ -39,14 +40,14 @@ const Contact = () => {
             <a href='https://api.whatsapp.com/send?phone=+919159550219'>Send a message</a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <label for="name">Name </label> 
           <input type="text" name="name" placeholder="Your Full Name" required/>
           <label for="email">Email</label>
           <input type="email" name="email" placeholder="Your Email"  required/>  
           <label for="message">Message</label>
           <textarea name="message" placeholder="Your Message"cols="20" rows="7" required></textarea>
-          <button type="submit" className='btn btn-primary'>Submit</button>
+          <input type="submit" className='btn btn-primary' value="Send" />
         </form>
       </div>
     </section>
