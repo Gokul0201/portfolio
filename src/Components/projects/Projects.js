70c39bbf319cad491c3e33af4ae587cff0251886
Gokul0201 <kgokula314@gmail.com>
2022-11-09 06:56:33 +0530

    modified

--- src/Components/projects/Projects.js
+++ src/Components/projects/Projects.js
@@ -10,6 +10,7 @@ const Projects = () => {
       id:'1',
       image:IMG1,
       title:'Inventory Billing',
+      description:"Inventory Billing app is app that helps to create Billing & Inventory Management easily.",
       github_FE:'https://github.com/Gokul0201/Inventory-Billing-FE.git',
       github_BE:'https://github.com/Gokul0201/Inventory-billing-BE.git',
       demo:'https://inevntory-billing.netlify.app/'
@@ -18,6 +19,7 @@ const Projects = () => {
       id:'2',
       image:IMG2,
       title:'POSTBOX',
+      description:"PostBox app allows you to communicate with your contacts through chat. It enables you to send and receive messages.",
       github_FE:'https://github.com/Gokul0201/Postbox-chat-FE.git',
       github_BE:'https://github.com/Gokul0201/PostBox_chat-BE.git',
       demo:'https://postbox-chat.netlify.app/'
@@ -26,6 +28,7 @@ const Projects = () => {
       id:'3',
       image:IMG3,
       title:'WEB SCRAPPING',
+      description:"Web scraping is an automatic method to obtain large amounts of data from websites .Data extracted from websites like Flipkart,Snapdeal.",
       github_FE:'https://github.com/Gokul0201/WebScrapper-Ecommerce-frontend.git',
       github_BE:'https://github.com/Gokul0201/WebScrapper-Ecommerce-backend.git',
       demo:'https://webscrape-ecommerce.netlify.app/'
@@ -34,6 +37,7 @@ const Projects = () => {
       id:'4',
       image:IMG4,
       title:'TENTKOTTA',
+      description:"Tentkotta App is an movie ticket booking app.",
       github_FE:'https://github.com/Gokul0201/Tentkotta-movie-booking-app-FE.git',
       github_BE:'https://github.com/Gokul0201/Tentkotta-movie-booking-app-BE.git',
       demo:'https://tentkotta.netlify.app/'
@@ -43,7 +47,11 @@ const Projects = () => {
     <section id="projects">
       <h5>My recent work</h5>
       <h2>Projects</h2>
-
+     <div className='credentials'>
+     <h5>Credentials</h5>
+      <h5>User: (username:demo123,password:12345678)</h5>
+      <h5>Admin: (username:admin123,password:12345678)</h5>
+      </div>
       <div className="container projects_container">{
         data.map((e) => {
           return(
@@ -52,9 +60,7 @@ const Projects = () => {
             <img src={e.image} alt=" "></img>
           </div>
           <h3>{e.title}</h3>
-          <p> Credentials:</p>
-          <p>Username:demo123</p>
-          <p>Password:12345678</p>
+          <p className="description">{e.description}</p>
           <div className="project_item-link">
           <a href={e.github_FE} className="btn" target='_blank' rel='noreferrer'>Front End</a>
           <a href={e.github_BE} className="btn" target='_blank' rel='noreferrer'>Back End</a>
