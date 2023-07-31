343f167fe7e4d618287ce99facab64a5066f391d
GOKULAKRISHNAN K <kgokula314@gmail.com>
2023-07-31 21:50:07 +0530

    Update Buttons.js

--- src/Components/home/Buttons.js
+++ src/Components/home/Buttons.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Buttons = () => {
   return <>
       <div className="buttons"> 
-      <a href="https://drive.google.com/file/d/19VCt7MTz6-P2_-aZYFenKLfBMQ98Licn/view?usp=sharing" className='btn' target='_blank' rel='noreferrer'>Resume</a> 
+      <a href="https://drive.google.com/file/d/1AJyD8WZoFStjR2bD998UcoMv5JWLONge/view?usp=sharing" className='btn' target='_blank' rel='noreferrer'>Resume</a> 
       <a href='#contact' className='btn btn-primary'>Hire me</a>
       </div>
   </>
